4f559800901112c0ea624cc373f35925e98025f8
Régis Andrade <regis@Regiss-MacBook-Pro.local>
2020-05-02 10:36:52 -0300

    Solve problem "fp-hello-world-n-times".

---
+++ fp-hello-world-n-times/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let sayHelloN times =
+    List.init times (function _ -> "Hello World")
+
+[<EntryPoint>]
+let main argv =
+    let times = Console.ReadLine() |> int
+
+    let messages = sayHelloN times
+
+    for message in messages do
+        printfn "%s" message
+
+    0
