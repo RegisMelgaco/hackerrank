2025264bdcacc36833760743166b274e38b0a4d1
Régis Andrade <regis@Regiss-MacBook-Pro.local>
2020-05-02 11:50:12 -0300

    Solve fp-reverse-a-list.

---
+++ fp-reverse-a-list/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let reverseList l =
+    List.rev l
+
+let readConsoleAsList =
+    let mutable line = Console.ReadLine()
+    let mutable lines = []
+
+    while not (isNull line) do
+        lines <- lines @ [line]
+        line <- Console.ReadLine()
+
+    lines
+
+[<EntryPoint>]
+let main argv =
+    let values = readConsoleAsList
+    for v in (reverseList values) do
+        printfn "%s" v
+
+    0
