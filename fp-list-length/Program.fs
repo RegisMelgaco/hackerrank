4279a6303fefa4e38aa444d20bdcdbc71bfd1452
Régis Andrade <regis@Regiss-MacBook-Pro.local>
2020-05-03 20:41:35 -0300

    Solve fp-list-length.

---
+++ fp-list-length/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+
+let readConsoleAsList =
+    let mutable line = Console.ReadLine()
+    let mutable lines = []
+
+    while not (isNull line) do
+        lines <- lines @ [line]
+        line <- Console.ReadLine()
+
+    lines
+
+let f list =
+    List.length list
+
+[<EntryPoint>]
+let main argv =
+    let list = readConsoleAsList
+    let length = f list
+
+    printf "%d" length
+
+    0
