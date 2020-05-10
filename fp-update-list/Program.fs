c84846f2256c0305aebcc43fa8a71c391adf2024
RegisMelgaco <regis.melgaco@gmail.com>
2020-05-10 11:01:48 -0300

    Solve fp-update-list.

---
+++ fp-update-list/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let f arr =
+    List.map
+        abs
+        arr
+
+let input = 
+    stdin.ReadToEnd().Split '\n' 
+    |> Array.map(fun x -> int(x)) 
+    |> Array.toList
+    
+let print_out (data:int list) = List.iter (fun x -> printfn "%A" x) data
+
+[<EntryPoint>]
+let main argv =
+    print_out(f input)
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
